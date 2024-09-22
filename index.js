f6e67d829e7f98b4b61f4abcab00b716ebe71be1
ARG RABBI <52894020+itrabbi24@users.noreply.github.com>
2024-09-22 11:51:02 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const path = require("path");
+// Serve static files from the 'public' directory
+app.use(express.static("public"));
+
+app.use(cors());
+app.use(express.json());
+
+app.use(cors());
+app.use(express.json());
